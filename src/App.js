4b4d368e5814e9dff8b22c1e16c85928b4875779
Jaanu423 <puja.mourya575@gmail.com>
2023-02-21 05:50:18 +0530

    blog first

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from 'react'
-import './App.css'
-import Header from './components/Header'
-import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    <Header/>
-    </>
+    <div>App</div>
   )
 }
 
